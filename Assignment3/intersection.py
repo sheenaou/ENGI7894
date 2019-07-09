a61497073eadb65732d978babfa8b8b12aa3bbc2
Sheena Ou <so7122@mun.ca>
2019-07-09 13:06:09 -0230

    finished part 1

--- Assignment3/intersection.py
+++ Assignment3/intersection.py
@@ -6,6 +6,8 @@ speed = 0.10
 action_time = 2
 active_light = None
 
+directions = {0:"left", 1:"straight", 2:"right"}
+
 class Car:
     def __init__(self, road, id, direction):
         self.road = road
@@ -29,9 +31,10 @@ class Road:
         self.set_directions()
         print("Road {} has been created with the following cars:".format(id, ))
         for car in self.cars:
-            print("\t> {} and wants to go {}".format(car.id, car.direction))
+            print("\t> {} and wants to go {}".format(car.id, directions[car.direction]))
 
     def set_directions(self):
+        """Method to determine which road corresponds to the left, straight, and right directions"""
         self.left = (self.id + 3) % 4
         self.straight = (self.id + 2) % 4
         self.right = (self.id + 1) % 4
@@ -41,31 +44,35 @@ class Road:
 
     def green_actions(self, direction):
         clock = RealtimeEnvironment(initial_time=0, factor=speed, strict=False)
+        print("Car {} is trying {}".format(self.cars[0].id, directions[self.cars[0].direction]))
         # CASE: Left turn
         if int(direction) == 0:
             if self.locks[str(self.left)].acquire():
+                print("Car {} acquired lock {} 1/2".format(self.cars[0].id, str(self.left)))
                 if self.locks[str(self.id)].acquire(timeout=action_time):
+                    print("Car {} acquired lock {} 2/2".format(self.cars[0].id, str(self.id)))
                     clock.run(until=action_time) # Two seconds to perform actions -- arbitrary
                     car = self.cars.pop(0)
                     print("Car {} has made a left turn".format(car.id))
                     self.locks[str(self.id)].release()
-                else:
-                    self.locks[str(self.left)].release()
+                self.locks[str(self.left)].release()
 
         # CASE: Straight
         elif int(direction) == 1:
             if self.locks[str(self.right)].acquire():
+                print("Car {} acquired lock {} 1/2".format(self.cars[0].id, str(self.right)))
                 if self.locks[str(self.straight)].acquire(timeout=action_time):
+                    print("Car {} acquired lock {} 2/2".format(self.cars[0].id, str(self.straight)))
                     clock.run(until=action_time)
                     car = self.cars.pop(0)
                     print("Car {} has driven straight".format(car.id))
                     self.locks[str(self.straight)].release()
-                else:
-                    self.locks[str(self.right)].release()
+                self.locks[str(self.right)].release()
 
         # CASE: Right turn
         elif int(direction) == 2:
             self.locks[str(self.right)].acquire()
+            print("Car {} acquired lock {} 1/1".format(self.cars[0].id, str(self.right)))
             clock.run(until=2)
             car = self.cars.pop(0)
             print("Car {} has made a right turn".format(car.id))
@@ -76,16 +83,16 @@ class Road:
             pass
 
     def red_actions(self, direction):
-        clock = RealtimeEnvironment(initial_time=0, factor=speed, strict=False)
         complete = False
         global active_light
         if int(direction) == 2:
             while self.id != (active_light.direction % 2) and not complete:
+                clock = RealtimeEnvironment(initial_time=0, factor=speed, strict=False)
                 clock.run(until=3)
                 if self.locks[str(self.right)].acquire(False):
                     clock.run(until=5)
                     car = self.cars.pop(0)
-                    print("Car {} has made a left turn".format(car.id))
+                    print("Car {} has made a right turn".format(car.id))
                     self.locks[str(self.right)].release()
                     complete = True
 
@@ -93,7 +100,8 @@ class Road:
         global active_light
         while self.status:
             if active_light is not None and len(self.cars) > 0:
-                if self.id == (active_light.direction % 2):   # The light is green for this road
+                if (self.id % 2) == active_light.direction:   # The light is green for this road
+                    print("Road {} is has a green light".format(self.id))
                     self.green_actions(direction=self.cars[0].direction)
                 else:
                     self.red_actions(direction=self.cars[0].direction)
@@ -166,4 +174,5 @@ class Intersection:
             road.start()
 
 t = Intersection()
+wait = input()
 t.run()
